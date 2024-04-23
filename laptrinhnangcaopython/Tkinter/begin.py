34f8bb98ab1d6639cbebcb3b9c2fed68e5578dac
mintrit29 <tongminhtriet1967@gmail.com>
2024-04-23 19:50:13 +0700

    git add. git commit

--- laptrinhnangcaopython/Tkinter/begin.py
+++ laptrinhnangcaopython/Tkinter/begin.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 #khai bao form chinh
 root = tkinter.Tk()
 #them widget
@@ -9,6 +10,9 @@ tkinter.Label(root,
     fg="red",
 ).pack()
 tkinter.Label(root, text="Phan Mem Quan Ly").pack()
-tkinter.Button(root, text="Lick me!").pack()
+tkinter.Button(root, 
+    text="Lick me!",
+    command=lambda : messagebox.showinfo("Thong bao","Vua click")
+).pack()
 #hien form
 root.mainloop()
