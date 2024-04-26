56ab1b65f417e8f3b9bcf6c85e66118b72844991
mintrit29 <tongminhtriet1967@gmail.com>
2024-04-26 20:35:37 +0700

    git add . git commit

---
+++ laptrinhnangcaopython/Tkinter/OnTkinter/beverageselector.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+def selected():
+    sugar = sugar_var.get()
+    ice = ice_var.get()
+    cream = cream_var.get()
+    if sugar:
+        sugar = 'Sugar'
+    else:
+        sugar = 'No Sugar'
+    if ice:
+        ice = 'Ice'
+    else:
+        ice = 'No Ice'
+    if cream:
+        cream = 'Cream'
+    else:
+        cream = 'No Cream'
+    label.config(text = f'Option selected are:\n {sugar}\n {ice}\n {cream}')
+
+root = Tk()
+root.geometry('300x400')
+
+sugar_var = BooleanVar()
+ice_var = BooleanVar()
+cream_var = BooleanVar()
+
+sugar_checkbox = Checkbutton(root,text='Sugar',variable=sugar_var,command=selected)
+ice_checkbox = Checkbutton(root,text='Ice',variable=ice_var,command=selected)
+cream_checkbox = Checkbutton(root,text='Cream',variable=cream_var,command=selected)
+sugar_checkbox.pack()
+ice_checkbox.pack()
+cream_checkbox.pack()
+
+label = Label(root)
+label.pack()
+
+root.mainloop()
