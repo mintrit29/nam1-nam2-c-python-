e0cec2f6d4717caf136927e141827efc23cd3580
mintrit29 <tongminhtriet1967@gmail.com>
2024-05-17 21:02:11 +0700

    git add . git commit

--- laptrinhnangcaopython/DeTaiCuoiKi/Test chuong trinh/sql.py
+++ laptrinhnangcaopython/DeTaiCuoiKi/Test chuong trinh/sql.py
@@ -9,52 +9,43 @@ cursor = conn.cursor()
 # Tạo bảng DOIBONG
 cursor.execute('''
 CREATE TABLE DOIBONG (
-    MaDB INTEGER PRIMARY KEY,
-    MaSH INTEGER,
-    TenDoi TEXT NOT NULL,
-    NamTL INTEGER,
-    GiaTriDB REAL,
-    FOREIGN KEY (MaSH) REFERENCES CHUSOHUU(MaSH)
-)
-''')
-
-# Tạo bảng CHUSOHUU
-cursor.execute('''
-CREATE TABLE CHUSOHUU (
-    MaSH INTEGER PRIMARY KEY,
-    HoTen TEXT NOT NULL,
-    Email TEXT UNIQUE,
-    NganSach REAL
-)
+  MaDB TEXT PRIMARY KEY,
+  TenDoi TEXT NOT NULL,
+  NamTL INTEGER,
+  GiaTriDB REAL
+);
 ''')
 
 # Tạo bảng CAUTHU
 cursor.execute('''
 CREATE TABLE CAUTHU (
-    MaCT INTEGER PRIMARY KEY,
-    TenCauThu TEXT NOT NULL,
-    MaDB INTEGER,
-    SoAo INTEGER,
-    ViTri TEXT,
-    NgaySinh DATE,
-    QuocTich TEXT,
-    ChieuCao REAL,
-    CanNang REAL,
-    GiaTriCT REAL,
-    FOREIGN KEY (MaDB) REFERENCES DOIBONG(MaDB)
-)
+  MaCT TEXT PRIMARY KEY,
+  TenCauThu TEXT NOT NULL,
+  MaDB TEXT NOT NULL,
+  SoAo INTEGER,
+  ViTri TEXT,
+  NgaySinh DATE,
+  QuocTich TEXT,
+  ChieuCao REAL,
+  CanNang REAL,
+  GiaTriCT REAL,
+  FOREIGN KEY (MaDB) REFERENCES DOIBONG(MaDB)
+);
 ''')
 
 # Tạo bảng CHUYENDICH
 cursor.execute('''
 CREATE TABLE CHUYENDICH (
-    MaGD INTEGER PRIMARY KEY,
-    MaCT INTEGER,
-    MaDB INTEGER,
-    LichSu TEXT,
+    MaGD TEXT PRIMARY KEY,
+    MaCT TEXT NOT NULL,
+    MaDB TEXT NOT NULL,
+    TenCauThu TEXT,
+    GiaTriCT REAL,
+    TenDoi TEXT,
+    NgayGiaoDich DATE,
     FOREIGN KEY (MaCT) REFERENCES CAUTHU(MaCT),
     FOREIGN KEY (MaDB) REFERENCES DOIBONG(MaDB)
-)
+);
 ''')
 
 # Lưu thay đổi và đóng kết nối
