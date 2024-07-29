520c1444dc7aaf92f8ab2680416576bd9f2b498f
mintrit29 <tongminhtriet1967@gmail.com>
2024-07-29 14:13:56 +0700

    git add . git commit

--- Web Developer/Thực hành trên lớp/ch09-lab-start-master/js/lab09-walkthrough05.js
+++ Web Developer/Thực hành trên lớp/ch09-lab-start-master/js/lab09-walkthrough05.js
@@ -1,12 +1,15 @@
-
 // this variable has global scope
-var boxClass = 'movingDiv';
+var boxClass = "movingDiv";
 
 function outputBox(num) {
-   var box = "<div class='" + boxClass + "' id='div" + num + "'>";
-   box += "This is div " + num;
-   box += "</div>";
-   return box;
+  var box = "<div class='" + boxClass + "' id='div" + num + "'>";
+  box += "This is div " + num;
+  box += "</div>";
+  return box;
 }
 
 /* your code goes here */
+window.addEventListener("load", function () {
+  var divToGet = document.getElementById("div1");
+  alert(divToGet.innerHTML);
+});
