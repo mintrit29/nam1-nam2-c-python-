496b148c895d2e0536bb13e4e351a177a1e80068
mintrit29 <tongminhtriet1967@gmail.com>
2024-08-05 18:23:32 +0700

    git add . git commit

---
+++ Web Developer/Thực hành trên lớp/ch10-lab-start-master/README.md
@@ -0,0 +1,7 @@
+# Fundamentals of Web Development, 2nd Edition
+### Chapter 10 [JavaScript 3: jQuery], Lab
+
+What You Will Learn
+* Extending JavaScript using jQuery
+* Manipulating the DOM and handling events using jQuery
+* Making asynchronous requests in JavaScript using jQuery
---
+++ Web Developer/Thực hành trên lớp/ch10-lab-start-master/js/lab10-walkthrough04.js
@@ -0,0 +1,14 @@
+$(function () {
+// chaining handlers
+$(".panel")
+.on("mousemove",function (e) {
+$("#message").html("x=" + e.pageX + " y=" + e.pageY);
+})
+.on("mouseleave",function (e) {
+$("#message").html("goodbye!");
+})
+.on("click",function () {
+$("#message").html("stopped move reporting");
+$(".panel").off("mousemove");
+});
+});
